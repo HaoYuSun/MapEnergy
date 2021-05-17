c4b0043736eed756d0ba217c93873f2b35bbea5e
sunhaoyu <shy@en.com.cn>
2021-05-17 16:34:25 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,16 @@
 // index.js
+// https://developers.weixin.qq.com/miniprogram/dev/extended/component-plus/index-list.html
 // 获取应用实例
 const app = getApp()
 
 // 引入SDK核心类
 var QQMapWX = require('../../qqmap-wx-jssdk.js');
-// 实例化API核心类
-var qqmapsdk = new QQMapWX({
-  key: 'QYEBZ-VCMED-DD44G-H4QVE-U7OHS-PAFAA' // 必填
-});
+
 Page({
   data: {
+    mpCtx: null,
+    inputModel: '',
+    inputFocus: '',
     ployline: [],//路线渲染
     latitude: 22.7340784793,//纬度
     longitude: 113.504708375,//经度
@@ -17,64 +18,148 @@ Page({
     markers: [{
       // iconPath: "../images/123.png",
       id: 0,
-      longitude: 113.504508375,
-      latitude: 22.7344784793,
+      longitude: 117.265348,
+      latitude: 38.907694,
       width: 20,
       height: 20,
     }],
+    polygons:[],
+    // polygons: [{
+    //   points: [{}],
+    //   fillColor: "#ffff0033",
+    //   strokeColor: "#FFF",
+    //   strokeWidth: 2,
+    //   zIndex: 1
+    // }]
 
   },
+  moveToLocation: function () {
+    var that = this;
+    wx.chooseLocation({
+       success: function (res) {
+         console.log(res)
+          let mobileLocation = {
+             longitude: res.longitude,
+             latitude: res.latitude,
+            //  address: res.address,
+          };
+          // that.setData({
+          //    mobileLocation: mobileLocation,
+          // });
+       },
+       fail: function (err) {
+          console.log(err)
+       }
+    });
+ },
   onLoad: function (options) {  
-    var _this = this;
-    //调用距离计算接口
-    qqmapsdk.direction({
-      mode: 'driving',//可选值：'driving'（驾车）、'walking'（步行）、'bicycling'（骑行），不填默认：'driving',可不填
-      //from参数不填默认当前地址
-      from:'22.7344784793,113.504708375',
-      to: '22.7342920943,108.594824432',
+    var that = this;
+    that.mpCtx = wx.createMapContext("map", this);
+    wx.getLocation({//默认为 wgs84 返回 gps 坐标，gcj02 返回可用于wx.openLocation的坐标
+      type: 'gcj02',
       success: function (res) {
-        console.log(res);
-        var ret = res;
-        var coors = ret.result.routes[0].polyline, pl = [];
-        //坐标解压（返回的点串坐标，通过前向差分进行压缩）
-        var kr = 1000000;
-        for (var i = 2; i < coors.length; i++) {
-          coors[i] = Number(coors[i - 2]) + Number(coors[i]) / kr;
-        }
-        //将解压后的坐标放入点串数组pl中
-        for (var i = 0; i < coors.length; i += 2) {
-          pl.push({ latitude: coors[i], longitude: coors[i + 1] })
-        }
-        console.log(pl)
-        //设置polyline属性，将路线显示出来,将解压坐标第一个数据作为起点
-        _this.setData({
-          latitude: pl[0].latitude,
-          longitude: pl[0].longitude,
-          polyline: [{
-            points: pl,
-            color: "#20B2AA", width: 4, dottedLine: false
-          
-          }]
+        that.setData({
+        /*赋值*/
+          latitude: res.latitude,
+          longitude: res.longitude
         })
-      },
-      fail: function (error) {
-        console.error(error);
-      },
-      complete: function (res) {
-        console.log(res);
       }
     });
+    // // 实例化API核心类
+    let qqmapsdk = new QQMapWX({
+      key: 'QYEBZ-VCMED-DD44G-H4QVE-U7OHS-PAFAA' // 必填
+    });
+    qqmapsdk.getLocation({
+      success: function (res) {
+        console.log(res);
+      },
+      fail: function (res) {
+          console.log(res);
+      },
+      complete: function (res) {
+          console.log(res);
+      }
+    });
+    // this.mapCtx = wx.createMapContext("map", this);
+    // this.mapCtx.getCenterLocation({
+    //   success: function (res) {
+    //       console.log(res)
+    //       that.setData({
+    //           longitude: res.longitude,
+    //           latitude: res.latitude,
+    //       })
+    //   }, 
+    //   fail: function (res) {
+    //         console.log(res)
+    //   }
+    // })
   },
-  //视野发生变化时触发
+  /**
+     * 将焦点给到 input（在真机上不能获取input焦点）
+     */
+    tapInput() {
+      this.setData({
+          //在真机上将焦点给input
+          inputFocus:true,
+          //初始占位清空
+          inputInfo: ''
+      });
+  },
+
+  /**
+   * input 失去焦点后将 input 的输入内容给到cover-view
+   */
+  blurInput(e) {
+      this.setData({
+          inputInfo: e.detail.value || '输入'
+      });
+  },
+
+  hometap:function(e){
+    console.log('123123');
+  },
+  righttap:function(e) {
+    var that = this;
+    wx.getLocation({
+      type: 'gcj02',
+      success: function (res) {
+        console.log(res);
+        that.mpCtx.moveToLocation();
+      }
+    })
+  },
+  _locationChangeFn(res) {
+    console.log('location change', res)
+   },
+  //视野发生变化时触发 移动
   regionchange(e) {
+    console.log(1)
     console.log(e.type)
+    // wx.chooseLocation({
+    //   success: function (res) {
+    //     console.log(res)
+    //      let mobileLocation = {
+    //         longitude: res.longitude,
+    //         latitude: res.latitude,
+    //        //  address: res.address,
+    //      };
+    //      // that.setData({
+    //      //    mobileLocation: mobileLocation,
+    //      // });
+    //   },
+    //   fail: function (err) {
+    //      console.log(err)
+    //   }
+  //  });
   },
   //单击标记点时触发
   markertap(e) {
+    console.log(2)
     console.log(e.markerId)
   },
   //单击空间上时触发
   controltap(e) {
+    console.log(3)
     console.log(e.controlId)
   }
 })
